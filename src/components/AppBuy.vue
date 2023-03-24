c563506831fff880d7b1c485cedf905482c00ea3
danilopiccinni <danilopiccinni@gmail.com>
2023-03-24 18:19:23 +0100

    fix: disposizione elementi AppBuy

--- src/components/AppBuy.vue
+++ src/components/AppBuy.vue
@@ -59,14 +59,14 @@
     ul {
         display: flex;
         align-items: center;
-        justify-content: space-between;
+        justify-content: space-around;
 
         div {
             display: flex;
             align-items: center;
             gap: 12px;
 
-            
+
             span {
                 color: white;
             }
