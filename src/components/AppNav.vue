98bd8463ab31025bf7aeddf5119669b24a70f4e8
danilopiccinni <danilopiccinni@gmail.com>
2023-03-24 15:19:45 +0100

    feat: component AppNav

---
+++ src/components/AppNav.vue
@@ -0,0 +1,66 @@
+<script>
+
+    export default {
+        data() {
+            return {
+                logo : '/img/dc-logo.png',
+                links : [
+                    'characters',
+                    'comics',
+                    'movies',
+                    'tv',
+                    'games',
+                    'collectibles',
+                    'videos',
+                    'fans',
+                    'news',
+                    'shop',
+                ]
+            }
+        }
+    }
+</script>
+
+<template>
+
+    <nav>
+
+        <div>
+            <img :src="logo" alt="">
+        </div>
+
+        <div>
+            <ul>
+                <li v-for="link in links">
+                    {{ link }}
+                </li>
+            </ul>
+        </div>
+
+    </nav>
+
+
+</template>
+
+<style scoped lang="scss">
+
+    nav {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding-top: 25px;
+        padding-bottom: 25px;
+
+        ul {
+            
+            display: flex;
+            
+            li {
+                text-transform: uppercase;
+                padding: 15px 15px;
+            }
+        }
+        
+    }
+
+</style>
