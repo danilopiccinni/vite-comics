144d08f5fba56f70d2684ec395d94ee39f8e8d93
danilopiccinni <danilopiccinni@gmail.com>
2023-03-27 15:22:27 +0200

    feat: AppComicBook(provvisorio in asssenza di img)

---
+++ src/components/AppComicBook.vue
@@ -0,0 +1,51 @@
+<script>
+    export default {
+        data() {
+            return{
+
+            }
+        },
+
+        props : {
+            image : String,
+            title : String
+        }
+    }
+</script>
+
+<template>
+
+    <div class="card">
+        <img :src="image" alt="">
+        <span> {{ title }} </span>
+    </div>
+
+</template>
+
+<style scoped lang="scss">
+
+    .card {
+        display: flex;
+        flex-flow: column nowrap;
+        gap: 15px;
+
+        width: calc(100% / 6 - 20px);
+        height: 400px;
+        padding: 10px;
+
+        border: 1px solid white;
+
+        img {
+            width: 100%;
+            height: 80%;
+            border: 1px solid white;
+        }
+
+        span {
+            text-transform: uppercase;
+            color: white;
+            font-weight: bold;
+        }
+    }
+
+</style>
